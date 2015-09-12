19289c7c35d1482c495709b06f4b5fe0201d7e91
tudou <tudou@tudoudeMacBook-Pro.local>
2015-09-12 11:36:10 +0800

    .

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var j = request.jar();
 var fs = require('fs');
 /*
 {
@@ -7,9 +8,8 @@ var fs = require('fs');
 	1:{CSS:[],JS:[]}
 }
 */
-var weeklyNum = 124;
+var weeklyNum = 125;
 var weekly = {};
-
 
 var reqUrl = 'http://www.75team.com/weekly/issue';
 
@@ -17,22 +17,54 @@ var reqUrl = 'http://www.75team.com/weekly/issue';
 	var callee = arguments.callee;
 	//callee(++index);
 	//console.log('send: ' + reqUrl + index + '.html');
-	request(reqUrl + index + '.html', function(err, response, body){
+	console.log('调用请求前：' + reqUrl + index + '.html');
+	var option = {
+		url: reqUrl + index + '.html',
+		
+		headers: {
+			"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+			"Accept-Language": "zh-cn,zh;q=0.5",
+			"Accept-Encoding": "utf-8;q=0.7,*;q=0.7",
+			"Referer": "http://www.baidu.com/",
+			"Cache-Control": "max-age=0",
+			'Cookie': '_gat=1; _ga=GA1.2.1291159534.1441994155',
+			'Connection': 'keep-alive',
+			'RA-Sid': '72DD9350-20150606-071542-d81a15-564661',
+			'RA-Ver': '3.0.7',
+			'Upgrade-Insecure-Requests': '1',
+			'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
+		}
+		//jar: j
+	};
+	option.headers = null;
+	request(option, function(err, response, body){
 		var $,
 			singlePageData;
 		if(err || response.statusCode != '200'){
 			console.log('request error: ' + reqUrl, err);
 			return;
 		}
+		console.log('调用请求后：' + index);
 		$ = cheerio.load(body, {decodeEntities: false});
 		singlePageData = parseStructor($);
-		fs.writeFile('./data.json', JSON.stringify(singlePageData), {encoding:'utf8'});
+		/*
+		fs.appendFileSync('./data.json', JSON.stringify(singlePageData), {encoding:'utf8'}, function(err){
+			if(err){
+				console.log('appendFile: ' + err);
+			}
+		});
+		*/
+		singlePageData = null;
+
 		//console.log('done: ' + reqUrl + index + '.html');
-		if(++index > 1){
-			return;
-		}else{
-			callee(index);
-		}
+		setTimeout(function(){
+			if(++index > weeklyNum){
+				return;
+			}else{
+				callee(index);
+			}
+		}, 100);
+		
 	});
 
 })(1);
@@ -59,9 +91,9 @@ function parseStructor($){
 */
 function getClasses($){
 	var classes = [];			// structor
+	var itemClasses = {article:[]};	// structor
 	var fPushArtical = false;	// 是否要添加文件标题和连接
 	$('#content ul li').each(function(index, elem){
-		var itemClasses = {article:[]};	// structor
 		// 是一个分类标题接下来就需要添加文章列表
 		$(elem).hasClass('article') === true ? fPushArtical = true : fPushArtical = false;
 		if(fPushArtical){
@@ -73,9 +105,12 @@ function getClasses($){
 			// 文章
 		}else{
 			// 分类标题
+			itemClasses = {article:[]};
 			itemClasses['title'] = trim($(elem).text());
+			classes.push(itemClasses);
+
 		}
-		classes.push(itemClasses);
+		
 	});
 	return classes;
 }
